d89452e6489e3b74f34130059bba12a6ffdf7b8e
arsel <arsel.Giliazov@urfu.me>
2022-10-25 14:44:51 +0500

    Добавил ренейм, и пустую функцию чтобы ренейм пропускал и создавал отдельный даталист

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_func(command):
     elif command == "ZIP":
         return zip_columns
     else:  # command == "RENAME":
-        return rename
+        return empty_method
 
 
 def split_column(df):
@@ -31,22 +31,30 @@ def zip_columns(df):
     pass
 
 
-def rename(df):
+def empty_method():
     pass
 
 
 # входные данные
-commands_data = [('SPLIT', ['ФИО'], ['Фамилия', 'Имя', 'Отчество'])]
+commands_data = [('SPLIT', ['ФИО'], ['Фамилия', 'Имя', 'Отчество']),
+                 ('RENAME', ['Диагноз (расшифровка)'], ['Диагноз']),
+                 ('RENAME', ['Диагноз (код)'], ['Код диагноза']),
+                 ('RENAME', ['Тип исследования'], ['Категория исследования']),
+                 ('RENAME', ['Адрес прописки пациента'], ['Адрес проживания'])]
 filename = 'C:\\Users\\Арсель\\Downloads\\файл для проекта по конвертации.xlsx'
 df_input = pd.read_excel(filename, 'исходный формат')
 corr_fields = pd.read_excel(filename, 'нужный формат').columns
 result = pd.DataFrame()
 skip = []
+rename_data = []
 # выполнение команд
 for command_data in commands_data:
     command = command_data[0]
     input = command_data[1]
     corr = command_data[2]
+    if command == 'RENAME':
+        rename_data.append(command_data)
+        continue
     for item in input:
         skip.append(item)
     func = get_func(command)
@@ -55,6 +63,9 @@ for command_data in commands_data:
     for i in range(len(corr)):
         result[corr[i]] = corr_columns[i]
 
+# rename
+dict_rename = {x[1][0]: x[2][0] for x in rename_data}
+df_input.rename(columns=dict_rename, inplace=True)
 # заполнение дата фрейма
 for field in corr_fields:
     if not result.__contains__(field):
